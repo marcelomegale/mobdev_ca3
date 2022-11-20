a60e1216dd88dc6077e7f33b3cd9b9a9720fcf60
marcelomegale <marcelomegale@gmail.com>
2022-11-20 21:45:01 +0000

    index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,19 @@ const routes: Routes = [
   {
     path: 'characters',
     loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
-  }
+  }/**,
+  {
+    path: 'films',
+    loadChildren: () => import('./pages/spaceships/spaceships.module').then( m => m.SpaceshipsPageModule)
+  },
+  {
+    path: 'specimen-details',
+    loadChildren: () => import('./pages/specimen-details/specimen-details.module').then( m => m.SpecimenDetailsPageModule)
+  },
+  {
+    path: 'spaceship-details',
+    loadChildren: () => import('./pages/spaceship-details/spaceship-details.module').then( m => m.SpaceshipDetailsPageModule)
+  } */
 ];
 
 @NgModule({
