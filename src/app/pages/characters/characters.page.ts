2f6ed64ca9d561d7a74937915ed0b2c4aa65a60b
marcelomegale <marcelomegale@gmail.com>
2022-11-22 13:16:33 +0000

    character page

--- src/app/pages/characters/characters.page.ts
+++ src/app/pages/characters/characters.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.page.html',
@@ -15,13 +16,15 @@ export class CharactersPage implements OnInit {
   constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
-    this.characters = this.http.get('https://breakingbadapi.com/api/characters?limit=10&offset=10');
+    this.characters = this.http.get('https://www.breakingbadapi.com/api/characters');
+    this.characters.subscribe(data => {
+      console.log('my characters: ', data);
+    });
   }
  
   openDetails(characters) {
-    let split = characters.url.split('/');
-    let char_id = split[split.length-2];
-    this.router.navigateByUrl(`/tabs/characters/${char_id}`);
+    
+    let id = characters.char_id;
+    this.router.navigateByUrl(`/tabs/characters/${id}`);
   }
-
 }
