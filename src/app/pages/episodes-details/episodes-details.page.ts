84f7ca7bd172a58ed197232e245e02de407fde9b
marcelomegale <marcelomegale@gmail.com>
2022-11-23 18:56:29 +0000

    episodes

--- src/app/pages/episodes-details/episodes-details.page.ts
+++ src/app/pages/episodes-details/episodes-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-episodes-details',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EpisodesDetailsPage implements OnInit {
 
-  constructor() { }
-
+  episodes: any;
+ 
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+ 
   ngOnInit() {
+    let epid = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get(`https://www.breakingbadapi.com/api/episodes/${epid}`).subscribe(res => {
+      this.episodes = res[0]; 
+    });
   }
+  
 
 }
--- src/app/pages/episodes/episodes.page.ts
+++ src/app/pages/episodes/episodes.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-episodes',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EpisodesPage implements OnInit {
 
-  constructor() { }
+  episodes: Observable<any>;
+ 
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.episodes = this.http.get('https://www.breakingbadapi.com/api/episodes');
+    this.episodes.subscribe(data => {
+      console.log('my episodes: ', data);
+    });
   }
-
+ 
+  openDetails(episodes) {
+    
+    let id = episodes.episode_id;
+    this.router.navigateByUrl(`/tabs/episodes/${id}`);
+  }
 }
