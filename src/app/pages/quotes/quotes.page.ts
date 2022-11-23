be9733d81e3d95ab548f5a963bed1826120e1f06
marcelomegale <marcelomegale@gmail.com>
2022-11-23 18:58:10 +0000

    quotes

--- src/app/pages/quotes/quotes.page.ts
+++ src/app/pages/quotes/quotes.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-quotes',
@@ -6,10 +11,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.page.scss'],
 })
 export class QuotesPage implements OnInit {
+  quotes: Observable<any>;
+ 
+  constructor(private router: Router, private http: HttpClient) { }
 
-  constructor() { }
 
   ngOnInit() {
+    this.quotes = this.http.get('https://www.breakingbadapi.com/api/quotes');
+    
+  
   }
 
 }
